4b95d86e3e197d68750bd47737f3943c0c7e36d8
Богдан Троценко <bvtrots@gmail.com>
2024-07-06 10:52:24 +0300

    Добавил props в компонент главной страницы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,11 @@
-
 import MainScreen from '../../pages/main-page/main-page';
 
+type AppProps = {
+  offersCount: number;
+};
 
-function App(){
-  return (
-    <MainScreen/>
-  );
+function App({offersCount}: AppProps): JSX.Element {
+  return <MainScreen offersCount={offersCount}/>;
 }
 
 export default App;
