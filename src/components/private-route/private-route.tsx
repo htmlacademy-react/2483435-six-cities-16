9a0ca20e67d3b266f54d713cd08235a573cc593a
Bogdan <bvtrots@gmail.com>
2024-07-10 08:40:48 +0300

    Создал компонент приватного маршрута

---
+++ src/components/private-route/private-route.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from 'react-router-dom';
+import { AuthorizationStatus } from '../../const';
+import { AppRoute } from '../../const';
+
+type PrivateRouteProps = {
+   authorizationStatus: AuthorizationStatus;
+   children: JSX.Element;
+}
+
+function PrivateRoute({ authorizationStatus, children }: PrivateRouteProps): JSX.Element {
+   return (
+      authorizationStatus === AuthorizationStatus.Auth
+         ? children
+         : <Navigate to={AppRoute.Login} />
+   );
+}
+
+export default PrivateRoute;
