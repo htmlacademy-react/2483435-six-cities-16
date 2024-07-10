50e516a74b50e5acda3d6e968c37815687ae79aa
Богдан Троценко <bvtrots@gmail.com>
2024-07-10 13:33:28 +0300

    Добавил приватный маршрут избранных вариантов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,12 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppProps } from '../../types/app-props-type';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import HomePage from '../../pages/home-page/home-page';
 import LoginPage from '../../pages/login-page/login-page';
 import OfferPage from '../../pages/offer-page/offer-page';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import Error404 from '../../pages/error-404-page/error-404-page';
-
+import PrivateRoute from '../private-route/private-route';
 
 function App({ offersCount }: AppProps): JSX.Element {
   return (
@@ -17,22 +16,17 @@ function App({ offersCount }: AppProps): JSX.Element {
           path={AppRoute.Main}
           element={<HomePage offersCount={offersCount} />}
         />
-        <Route
-          path={AppRoute.Login}
-          element={<LoginPage />}
-        />
-        <Route
-          path={AppRoute.Offer}
-          element={<OfferPage />}
-        />
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path={AppRoute.Offer} element={<OfferPage />} />
         <Route
           path={AppRoute.Favorites}
-          element={<FavoritesPage />}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
         />
-        <Route
-          path="*"
-          element={<Error404 />}
-        />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
