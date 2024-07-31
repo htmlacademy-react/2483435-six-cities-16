f5f56f5dabfcfb70ae5cdcafc706c910d7096dd9
Богдан <159007823+b-trots@users.noreply.github.com>
2024-07-31 20:21:01 +0300

    Добавил хуки смены городов и маркеров (#6)

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 export const Setting = {
-  OffersCount: 5,
+  OffersCount: 30,
 } as const;
 
 export enum AppRoute {
@@ -7,17 +7,21 @@ export enum AppRoute {
   Login = '/login',
   Favorites = '/favorites',
   Offer = '/offer/:id',
-
 }
 
 export const RATING = [
-  {value: 5, text: 'perfect'},
-  {value: 4, text: 'good'},
-  {value: 3, text: 'not bad'},
-  {value: 2, text: 'badly'},
-  {value: 1, text: 'terrible'}
+  { value: 5, text: 'perfect' },
+  { value: 4, text: 'good' },
+  { value: 3, text: 'not bad' },
+  { value: 2, text: 'badly' },
+  { value: 1, text: 'terrible' },
 ];
 
 export const AUTH_STATUS = ['AUTH', 'NO_AUTH', 'UNKNOWN'];
 
 export type AuthStatus = 'AUTH' | 'NO_AUTH' | 'UNKNOWN';
+
+export enum MapMarker {
+  DefaultMarker = '../markup/img/pin.svg',
+  ActiveMarker = '../markup/img/pin-active.svg',
+}
--- src/pages/offer/utils.ts
+++ src/pages/offer/utils.ts
@@ -1,23 +1,9 @@
-import { Offer } from '../../types/offer-type';
+import { FullOffer } from '../../types/offer-type';
 
 import { Comment } from '../../types/comment-type';
 
-export const getCurrentOffer = (offers: Offer[], offerId: string) =>
-  offers.find((offer: Offer) => offer.id === offerId);
-
-// export const getNearOffers = (offers: ThumbnailOffer[], offerId: string) => {
-//   const currentCity = offers.find(
-//     (offer: ThumbnailOffer) => offer.id === offerId
-//   )!.city.name;
-//   const nearOffers: ThumbnailOffer[] = [];
-
-//   offers.map(
-//     (offer: ThumbnailOffer) =>
-//       offer.city.name === currentCity && nearOffers.push(offer)
-//   );
-
-//   return nearOffers;
-// };
+export const getCurrentOffer = (offers: FullOffer[], offerId: string) =>
+  offers.find((offer: FullOffer) => offer.id === offerId);
 
 export const sortByDate = (reviews: Comment[]) => reviews.sort(
   (date1, date2) =>
